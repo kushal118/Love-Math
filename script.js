b35a79e7b871b476ffff9806d81eaf1c162d3570
kushal118 <irfankushal02@gmail.com>
2021-03-09 00:30:38 +0000

    add checkAnswer

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let button of buttons) {
         button.addEventListener("click", function () {
             if(this.getAttribute("data-type") === "submit") {
-                alert("you have clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -17,11 +17,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 function runGame(gameType){
-let num1 = Math.floor(Math.random()*25)
-let num2 = Math.floor(Math.random()*25)
+let num1 = Math.floor(Math.random()*25)+1
+let num2 = Math.floor(Math.random()*25)+1
 
-if(gameType==="addition"){
-    displayAddditionalQuestion(num1,num2);
+if(gameType === "addition"){
+    displayAdditionQuestion(num1,num2);
 
 } else {
     alert(`unknown game type ${gameType}`)
@@ -30,9 +30,28 @@ if(gameType==="addition"){
 
 }
 function checkAnswer(){ 
-
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculatedAnswer = calculateCorrectAnswer()
+    let isCorrect = userAnswer === calculatedAnswer[0]
+ if (isCorrect){
+     alert('Hey! you got it Right :D');
+ }else{
+     alert(`Awwwww.......you answered ${userAnswer}.The correct Answer is ${calculatedAnswer[0]}`)
+ }
+ runGame(calculatedAnswer[1]);
 }
 function calculateCorrectAnswer(){
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator =document.getElementById('operator').innerText;
+    
+    if(operator === "+"){
+        return [operand1 + operand2,"addition"];
+    } else {
+        alert(`unimplemented operator ${operator}`)
+        throw `unimplemented operator ${operator}, aborting!`
+
+    }
 
 }
 function incrementScore(){
@@ -41,7 +60,7 @@ function incrementScore(){
 function incrementWrongAnswer(){
 
 }
-function displayAddditionalQuestion(operand1,operand2){
+function displayAdditionQuestion(operand1, operand2){
     document.getElementById('operand1').textContent = operand1
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+"
