2f5b5bf48bcf83f61c335e0b188dd851729ddcdb
kushal118 <irfankushal02@gmail.com>
2021-03-13 08:26:08 +0000

    add division question

--- script.js
+++ script.js
@@ -33,7 +33,9 @@ if(gameType === "addition"){
     displayMultiplyQuestion(num1,num2);
 } else if(gameType === "substract"){
     displaySubstractQuestion(num1,num2);
-} else {
+}else if(gameType === "division"){
+    displayDivisionQuestion(num1,num2);
+}else {
     alert(`unknown game type ${gameType}`)
     throw `unknown game type ${gameType},aborting`
 }
@@ -64,6 +66,8 @@ function calculateCorrectAnswer(){
 
     }else if(operator ==="-"){
         return [operand1 - operand2,"substract"]
+    }else if(operator ==="/"){
+        return [operand1 / operand2,"division"]
     }else {
         alert(`unimplemented operator ${operator}`)
         throw `unimplemented operator ${operator}, aborting!`
@@ -100,4 +104,11 @@ function displayMultiplyQuestion(operand1, operand2){
     document.getElementById('operand1').textContent = operand1
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x"
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+	document.getElementById("operand1").textContent = operand1*operand2;
+	document.getElementById("operand2").textContent = operand2;
+	document.getElementById("operator").textContent = "/";
 }
